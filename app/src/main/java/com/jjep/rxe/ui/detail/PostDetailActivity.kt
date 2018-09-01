6a4f5dac88d0235bddc589b6e9c58b47b040c72e
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2018-09-01 23:22:20 +0800

    WIP: Post detail

---
+++ app/src/main/java/com/jjep/rxe/ui/detail/PostDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.jjep.rxe.ui.detail
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jjep.rxe.R
+
+class PostDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_detail)
+    }
+}
